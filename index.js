59dd70c34eeb2df1268fa83e947ad60dd40b331f
Lily-McDaniel <70973463+Lily-McDaniel@users.noreply.github.com>
2021-03-14 18:30:34 -0400

    Add files via upload
    index files, resume, js

---
+++ index.js
@@ -0,0 +1,34 @@
+// for the featured projects
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// thank you display after submitting contact info
+function thankYou(){
+        var x = document.getElementById("contactForm");
+        var text = "Thank you, " + x.elements[0].value + " for reaching out! Looking forward to connecting with you.";
+
+        document.getElementById("thanks").innerHTML = text;
+      }
